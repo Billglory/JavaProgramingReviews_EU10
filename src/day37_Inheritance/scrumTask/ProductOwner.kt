f930983578bbaa16ff9849899ea82e0fbe3e6b0d
Billglory <113251889+Billglory@users.noreply.github.com>
2022-12-19 04:24:05 +0300

    Delete ProductOwner.kt

--- src/day37_Inheritance/scrumTask/ProductOwner.kt
+++
@@ -1,3 +0,0 @@
-package day37_Inheritance.scrumTask
-
-class ProductOwner(name: String, age: Int, gender: Char, jobTitle: String, id: Int, salary: Double, companyName: String) : Employee(name, age, gender, jobTitle, id, salary, companyName)
