7b0221843a44874b169e68450ec776146eda1dac
Billglory <113251889+Billglory@users.noreply.github.com>
2022-12-19 04:23:39 +0300

    Delete Developer.kt

--- src/day37_Inheritance/scrumTask/Developer.kt
+++
@@ -1,7 +0,0 @@
-package day37_Inheritance.scrumTask
-
-class Developer(name: String, age: Int, gender: Char, jobTitle: String, id: Int, salary: Double, companyName: String) : Employee(name, age, gender, jobTitle, id, salary, companyName) {
-    fun fixBugs() {
-        println((jobTitle + " " + name).toString() + " is crying")
-    }
-}
