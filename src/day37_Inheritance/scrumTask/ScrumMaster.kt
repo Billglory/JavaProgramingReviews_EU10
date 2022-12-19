6be059a127c56e554545aacfa37e994acbf352e3
Billglory <113251889+Billglory@users.noreply.github.com>
2022-12-19 04:24:13 +0300

    Delete ScrumMaster.kt

--- src/day37_Inheritance/scrumTask/ScrumMaster.kt
+++
@@ -1,3 +0,0 @@
-package day37_Inheritance.scrumTask
-
-class ScrumMaster(name: String, age: Int, gender: Char, jobTitle: String?, id: Int, salary: Double, companyName: String) : Employee(name, age, gender, "Scrum Master", id, salary, companyName)
