a7f47528430cbf5336ce1bd507d6567a052984ff
Billglory <113251889+Billglory@users.noreply.github.com>
2022-12-19 04:23:32 +0300

    Delete BusinessAnalyst.kt

--- src/day37_Inheritance/scrumTask/BusinessAnalyst.kt
+++
@@ -1,7 +0,0 @@
-package day37_Inheritance.scrumTask
-
-class BusinessAnalyst(name: String, age: Int, gender: Char, jobTitle: String, id: Int, salary: Double, companyName: String) : Employee(name, age, gender, jobTitle, id, salary, companyName) {
-    fun analyze() {
-        println(name + " is analyzing the documents")
-    }
-}
