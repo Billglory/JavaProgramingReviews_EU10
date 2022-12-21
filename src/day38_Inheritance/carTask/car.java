f69bb921b2268a1d50afc8f11015ad9ddac1a799
Bill <billglory90@gmail.com>
2022-12-21 12:30:41 +0300

    day38_Inheritance Continue-Method Overriding

--- src/day38_Inheritance/carTask/car.java
+++ src/day38_Inheritance/carTask/car.java
@@ -1,5 +1,60 @@
 package day38_Inheritance.carTask;
 
 public class car {
+
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+    public car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
     
+    public void start(){
+        System.out.println(brand +" "+ model + " is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand +" "+ model + " is driving");
+    }
+
+    public String toString() {
+        return "car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+
+   /*
+    public void fly(){
+
+    }
+    fly() is not common for all the cars, and parent class should only contain the common feature of all the subclasses
+    */
+
+
 }
+
+
+
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+				add a constructor to set all the fields
+			instance methods:
+				start(), drive(),toString()
+
+ */
