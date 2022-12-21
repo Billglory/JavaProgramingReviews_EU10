013ed846e3710ed77e5a86ee884a8f4c0db10ad2
Bill <billglory90@gmail.com>
2022-12-21 14:57:10 +0300

    day38:Inheritance Continue-Method Overriding

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,6 +1,6 @@
 package day38_Inheritance.carTask;
 
-public class Toyota extends car{
+public class Toyota extends Car{
 
     public Toyota(String model, int year, double price, String color, int miles) {
         super("Toyota", model, year, price, color, miles);
