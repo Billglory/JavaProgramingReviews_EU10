a18317894c154f4cefd83862585acc32c17bae39
Bill <billglory90@gmail.com>
2022-12-21 12:56:24 +0300

    day38:Inheritance Continue-Method Overriding

---
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -0,0 +1,21 @@
+package day38_Inheritance.carTask;
+
+public class Tesla extends car {
+
+    public Tesla(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand+" "+model+" in autopilot mode");
+    }
+
+
+    /*
+    @Override
+    public void start() {
+        System.out.println("Say \"Start\" to start "+brand+" "+model);
+    }
+    
+     */
+}
