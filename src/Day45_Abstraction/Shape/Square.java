97647408d3857ceea2bc01658f969882085580cd
Bill <billglory90@gmail.com>
2023-01-02 13:08:06 +0300

    Day45:Abstraction

--- src/Day45_Abstraction/Shape/Square.java
+++ src/Day45_Abstraction/Shape/Square.java
@@ -1,4 +1,41 @@
 package Day45_Abstraction.Shape;
 
-public class Square {
+public class Square extends Shape {
+
+
+    private double side;
+
+    public Square(final String name, final double side) {
+        super("Square");
+       setSide(side);
+    }
+
+    public double getSide() {
+        return this.side;
+    }
+
+    public void setSide(final double side) {
+        if (side <= 0) {
+            throw new RuntimeException("Invalid Side: " + side);
+        }
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                super.toString()+
+                "side=" + side +
+                '}';
+    }
 }
