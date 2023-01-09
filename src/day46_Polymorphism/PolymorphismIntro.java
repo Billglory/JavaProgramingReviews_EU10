47685231e272ab0b272f49900696ec9cd89cc3f5
Bill <billglory90@gmail.com>
2023-01-09 13:31:05 +0300

    day46:Polymorphism

--- src/day46_Polymorphism/PolymorphismIntro.java
+++ src/day46_Polymorphism/PolymorphismIntro.java
@@ -2,6 +2,9 @@ package day46_Polymorphism;
 
 import Day45_Abstraction.Shape.Circle;
 import Day45_Abstraction.Shape.Square;
+import day43_Abstraction.Employee.*;
+
+import java.util.ArrayList;
 
 public class PolymorphismIntro {
     public static void main(String[] args) {
@@ -18,8 +21,32 @@ public class PolymorphismIntro {
         Object[] array = {str, n1, n2, r1, new Circle(4) , new Square(7)};
 
 
+        /*
+            WebDriver driver;
+            driver = new ChromeDriver();
+            driver = new FireFoxDriver();
+         */
 
-        }
+
+        Employee tester = new Tester("Ali",30,'M',42,"SDET",145000.00);
+        Employee developer = new Developer("Alex",28,'M',32,"Web Developer",155000.00);
+        Employee teacher = new Teacher("Angel",28,'F',22,"English Teacher",85000.00);
+        Employee driver = new Driver("Jhon",34,'M',42,"Lyft Driver",75000.00);
+
+        ArrayList<Person> cydeoEmployees = new ArrayList();
+
+
+
+        cydeoEmployees.add(tester);
+        cydeoEmployees.add(developer);
+        cydeoEmployees.add(teacher);
+        cydeoEmployees.add(driver);
+
+
+        System.out.println("-------------------------------------------");
+
+        Employee employee = new Tester("Ali",30,'M',42,"SDET",145000.00);
+    }
     }
 
 
