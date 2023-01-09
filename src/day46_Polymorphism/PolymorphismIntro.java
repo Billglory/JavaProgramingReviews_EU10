69b43da1e8483309e044ddb840ed87482fbad5f5
Bill <billglory90@gmail.com>
2023-01-09 13:07:47 +0300

    day46:Polymorphism

---
+++ src/day46_Polymorphism/PolymorphismIntro.java
@@ -0,0 +1,26 @@
+package day46_Polymorphism;
+
+import Day45_Abstraction.Shape.Circle;
+import Day45_Abstraction.Shape.Square;
+
+public class PolymorphismIntro {
+    public static void main(String[] args) {
+
+
+        String str = "Wooden Spoon";
+
+        Integer n1 = 2;
+
+        Double n2 = 5.5;
+
+        Boolean r1 = true;
+
+        Object[] array = {str, n1, n2, r1, new Circle(4) , new Square(7)};
+
+
+
+        }
+    }
+
+
+
