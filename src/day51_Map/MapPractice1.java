162fd83e53e12e72fdb62fa6c3b3d57f00bd9b35
Bill <billglory90@gmail.com>
2023-01-27 19:26:11 +0300

    day51_Map

--- src/day51_Map/MapPractice1.java
+++ src/day51_Map/MapPractice1.java
@@ -24,14 +24,7 @@ public class MapPractice1 {
 
         //  1. Update the "M" to Male and "F" to Female
 
-
-        //  2. Display the names of all female employees
-    }
-}
-/*
-
-/*
-        for (String key : employeeMap.keySet()) {
+ /*       for (String key : employeeMap.keySet()) {
             if(employeeMap.get(key).equalsIgnoreCase("m") ){
                 employeeMap.replace(key, "Male");
             }
@@ -39,42 +32,35 @@ public class MapPractice1 {
                 employeeMap.replace(key, "Female");
             }
         }
-*/
-/*
+        System.out.println(employeeMap);
+  */
+
         for (Map.Entry<String, String> entry : employeeMap.entrySet()) {
-        if(entry.getValue().equalsIgnoreCase("m") ){
-        entry.setValue("Male");
-        //    employeeMap.replace(entry.getKey() , "Male");
+            if(entry.getValue().equalsIgnoreCase("m") ){
+                entry.setValue("Male");
+                //    employeeMap.replace(entry.getKey() , "Male");
+            }
+            if(entry.getValue().equalsIgnoreCase("f") ){
+                entry.setValue("Female");
+                //    employeeMap.replace(entry.getKey() , "Female");
+            }
         }
-
-        if(entry.getValue().equalsIgnoreCase("f") ){
-        entry.setValue("Female");
-        //    employeeMap.replace(entry.getKey() , "Female");
-        }
-
-
-        }
-
 
         System.out.println(employeeMap);
 
         System.out.println("--------------------------------------");
 
         //  2. Display the names of all female employees
-
         for (Map.Entry<String, String> eachPair : employeeMap.entrySet()) {
-        String eachKey = eachPair.getKey();
-        String eachValue = eachPair.getValue();
+            String eachKey = eachPair.getKey();
+            String eachValue = eachPair.getValue();
 
-        if(eachValue.equals("Female")){
-        System.out.println(eachKey);
-        }
-
-        }
-
-
-
+            if(eachValue.equals("Female")){
+                System.out.println(eachKey);
+            }
 
         }
 
- */
+
+    }
+}
